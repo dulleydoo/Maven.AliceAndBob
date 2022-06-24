d0bea88f1ef7da586cbc6ea7761f53e234ecd592
dulleydoo <107411441+dulleydoo@users.noreply.github.com>
2022-06-24 13:44:26 -0400

    Created if/else statement scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Your Name");
+        String name = scan.nextLine();
 
-    public static void main(String[] args ){
+
+        if (name.equals("Bob") | name.equals("Alice")) {
+            System.out.println("Hello There");
+
+
+
+        }
+        else {
+            System.out.println("You dont belong here");
+
+        }
+
+
+
+
 
     }
 }
